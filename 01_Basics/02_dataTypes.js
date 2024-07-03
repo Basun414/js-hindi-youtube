49616fcdd9562885ba5338f5faa0a349377d5842
Basun Kundu <137437375+Basun414@users.noreply.github.com>
2024-07-03 09:11:04 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict" //treat all JS Code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3+3)
+console.log("Hitesh")
+
+let name ="Hitesh"
+let age = 18
+let isloggedIn = false
+let state = null; //it is not undefined it is empty
+
+//number => 2^53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => example --- let state
+
+console.log(typeof null) //---object
+console.log(typeof undefined);
