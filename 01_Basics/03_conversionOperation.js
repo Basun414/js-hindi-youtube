e9789781004226b11b62cc26be5fb90556892f1a
Basun Kundu <137437375+Basun414@users.noreply.github.com>
2024-07-03 09:49:42 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+// let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score) //converts string into number but be careful as it can also convert like 33abc to number but its value will be displayed as Nan
+// console.log(typeof valueInNumber); //number
+// console.log(valueInNumber);
+
+// let score = null
+// let valueInNumber = Number(score)
+// console.log(valueInNumber); //value displayed will be 0 and if undefined is there the value will be Nan and for true --- 1, false --- 0, string = "Hitesh"---Nan
+
+//"33" => 33
+//"33abc" => Nan
+//true => 1
+//false => 0
+//"Basun Kundu" => true
+
+// let isLoggedIn = "Basun Kundu"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber)
+console.log(typeof (stringNumber));
