eda88a995b4eae5ce59d969e4806b95f08a8ea3c
Basun Kundu <137437375+Basun414@users.noreply.github.com>
2024-07-03 06:43:15 +0000

    learned about let var and const

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "basun@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//accountId = 2 //not allowed 
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengalauru"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
