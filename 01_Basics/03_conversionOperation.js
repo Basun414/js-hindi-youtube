b5f7adc9b474a4a67b548fc4640d4dbf3df3010b
Basun Kundu <137437375+Basun414@users.noreply.github.com>
2024-07-05 06:01:54 +0000

    learned about old code writing techniques and how not to write a code

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -22,7 +22,40 @@
 // let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn)
 
-let someNumber = 33
-let stringNumber = String(someNumber);
-console.log(stringNumber)
-console.log(typeof (stringNumber));
+// let someNumber = 33
+// let stringNumber = String(someNumber);
+// console.log(stringNumber)
+// console.log(typeof (stringNumber));
+
+//* *********Operations*******
+
+let value = 3;
+let negvalue = - value
+//console.log(negvalue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2)  //12
+// console.log(1 + "2") //12   
+// console.log("1" + "2" + 2); //12
+// console.log(1 + 2 + "2") //32 because of primitive type operation, don't go to deep in it
+
+// console.log(+true); //1
+// console.log(+""); //0
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
