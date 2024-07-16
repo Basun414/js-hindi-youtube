6da1fae2aa0fb02c53be3b6fad15482616e70477
Basun Kundu <137437375+Basun414@users.noreply.github.com>
2024-07-16 11:42:53 +0000

    learing about strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,6 @@
+const name = "Hitesh"
+const repoCount = 50
+
+//console.log(name + repoCount + "Value");  //old method no one uses now a days
+console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`); //string interpolation
+const gameName = new String('hiteshc');
